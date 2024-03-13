504ff76d938b50b4ed69ee2f9193c24f05bb51c0
Daniel Kim <developer.danielkim@gmail.com>
2024-03-12 18:24:09 -0700

    added character personal section ui test

--- buildSrc/src/main/kotlin/AndroidTestDependencies.kt
+++ buildSrc/src/main/kotlin/AndroidTestDependencies.kt
@@ -1,1 +1,7 @@
-object AndroidTestDependencies {}
+object AndroidTestDependencies {
+
+    const val COMPOSE_UI_TEST = "androidx.compose.ui:ui-test-junit4"
+
+    // Needed for createAndroidComposeRule (access to Activity), but not createComposeRule.
+    const val COMPOSE_TEST_MANIFEST = "androidx.compose.ui:ui-test-manifest"
+}
